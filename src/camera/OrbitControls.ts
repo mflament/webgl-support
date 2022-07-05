5689c428b0f1a48896ed9ee19ad97c1a82de6a02
Marc <marcflament@free.fr>
2022-07-05 22:35:36 +0200

    Some stuff

---
+++ src/camera/OrbitControls.ts
@@ -0,0 +1,147 @@
+import {PerspectiveCamera} from "./PerspectiveCamera";
+import {vec2, vec3} from "gl-matrix";
+import * as GLMath from "../GLMath";
+
+export enum MouseButton {
+    LEFT,
+    MIDDLE,
+    RIGHT,
+}
+
+export enum Action {
+    NONE,
+    PAN,
+    ROTATE
+}
+
+export class OrbitControls {
+    readonly mouseBindings: Record<MouseButton, Action> = {
+        [MouseButton.LEFT]: Action.ROTATE,
+        [MouseButton.RIGHT]: Action.PAN,
+        [MouseButton.MIDDLE]: Action.NONE
+    };
+
+    readonly inclinationRange: [number, number] = [0.01, Math.PI / 2 + 0.01];
+
+    private currentAction: Action = Action.NONE;
+    // cartesian position of camera around target
+    private readonly cpos = vec3.create();
+    // spherical position of camera around target
+    private readonly spos = vec3.create();
+
+    private readonly _vmove = vec2.create();
+
+    private readonly panx = vec3.create();
+    private readonly pany = vec3.create();
+
+    onChange?: () => void;
+
+    rotateSpeed = Math.PI;
+    panSpeed = 5;
+
+
+    constructor(readonly camera: PerspectiveCamera, readonly canvas: HTMLCanvasElement) {
+        canvas.addEventListener('mousedown', e => this.mouseDown(e as MouseEvent));
+        canvas.addEventListener('mousemove', e => this.mouseMove(e as MouseEvent));
+        canvas.addEventListener('mouseup', () => this.mouseUp());
+        canvas.addEventListener('wheel', e => this.mouseWheel(e as WheelEvent));
+        canvas.addEventListener('mouseleave', () => this.mouseLeave());
+        canvas.addEventListener('contextmenu', e => e.preventDefault());
+
+        vec3.sub(this.cpos, camera.position, camera.target);
+        GLMath.toSpherical(this.spos, this.cpos);
+    }
+
+    private mouseDown(e: MouseEvent): void {
+        const action = this.mouseBindings[e.button as MouseButton];
+        if (action !== Action.NONE) {
+            this.currentAction = action;
+            this.canvas.requestPointerLock();
+        }
+    }
+
+    private mouseMove(e: MouseEvent): void {
+        if (this.currentAction === Action.PAN)
+            this.pan(e);
+        else if (this.currentAction === Action.ROTATE)
+            this.rotate(e);
+    }
+
+    private mouseWheel(e: WheelEvent): void {
+        this.zoom(e.deltaY > 0);
+    }
+
+    private mouseUp(): void {
+        if (this.currentAction !== Action.NONE) {
+            document.exitPointerLock();
+            this.currentAction = Action.NONE;
+        }
+    }
+
+    private mouseLeave(): void {
+        this.currentAction = Action.NONE;
+    }
+
+    private rotate(e: MouseEvent): void {
+        const m = this.getMove(e, this.rotateSpeed);
+
+        const spos = this.spos;
+        let inclination = spos[1];
+        inclination = GLMath.clamp(inclination - m[1], this.inclinationRange[0], this.inclinationRange[1]);
+        this.spos[1] = inclination;
+
+        let azimuth = spos[2];
+
+        azimuth = azimuth - m[0];
+        if (azimuth > Math.PI) azimuth = azimuth - 2 * Math.PI;
+        else if (azimuth < -Math.PI) azimuth = azimuth + 2 * Math.PI;
+        this.spos[2] = azimuth;
+
+        GLMath.fromSpherical(this.cpos, this.spos);
+        vec3.add(this.camera.position, this.camera.target, this.cpos);
+        this.updateWorld();
+    }
+
+    private zoom(z: boolean): void {
+        let radius = this.spos[0];
+        const zoomFactor = 0.8;
+        radius += z ? zoomFactor : -zoomFactor;
+        radius = Math.max(this.camera.near, radius);
+        this.spos[0] = radius;
+
+        GLMath.fromSpherical(this.cpos, this.spos);
+        vec3.add(this.camera.position, this.camera.target, this.cpos);
+        this.updateWorld();
+    }
+
+    private pan(e: MouseEvent): void {
+        const {panx, pany, panSpeed, camera} = this;
+
+        GLMath.projectOnPlane(pany, this.cpos, camera.up);
+        vec3.negate(pany, vec3.normalize(pany, pany));
+
+        vec3.cross(panx, pany, camera.up);
+
+        const m = this.getMove(e, panSpeed);
+
+        vec3.scaleAndAdd(camera.position, camera.position, panx, m[0]);
+        vec3.scaleAndAdd(camera.position, camera.position, pany, -m[1]);
+
+        vec3.scaleAndAdd(camera.target, camera.target, panx, m[0]);
+        vec3.scaleAndAdd(camera.target, camera.target, pany, -m[1]);
+
+        this.updateWorld();
+    }
+
+    private updateWorld() {
+        this.camera.updateWorldMatrix();
+        this.onChange && this.onChange();
+    }
+
+    private getMove(e: MouseEvent, scale = 1): vec2 {
+        this._vmove[0] = e.movementX / (this.canvas.width * .5 / window.devicePixelRatio) * scale;
+        this._vmove[1] = e.movementY / (this.canvas.height * .5 / window.devicePixelRatio) * scale;
+        return this._vmove;
+    }
+
+}
---
+++ src/camera/PerspectiveCamera.ts
@@ -0,0 +1,74 @@
+import {mat4, ReadonlyVec2, vec3, vec4} from 'gl-matrix';
+import {toRad} from "../GLMath";
+
+export interface Ray {
+    readonly origin: vec3,
+    readonly dir: vec3
+}
+
+export class PerspectiveCamera {
+    readonly position = vec3.set(vec3.create(), 0, 0, 4);
+    readonly target = vec3.create();
+    readonly up = vec3.set(vec3.create(), 0, 1, 0);
+
+    aspect = 1;
+    fovx = toRad(75);
+    near = 0.00001;
+    far = 1000;
+
+    private readonly _worldMatrix = mat4.create();
+    private readonly _invWorldMatrix = mat4.create();
+
+    private readonly _projectionMatrix = mat4.create();
+    private readonly _invProjectionMatrix = mat4.create();
+
+    constructor(config?: Partial<PerspectiveCamera>) {
+        if (config?.position) vec3.copy(this.position, config?.position);
+        if (config?.target) vec3.copy(this.target, config?.target);
+        if (config?.up) vec3.copy(this.up, config?.up);
+        if (config?.aspect !== undefined) this.aspect = config.aspect;
+        if (config?.fovx !== undefined) this.fovx = config.fovx;
+        if (config?.near !== undefined) this.near = config.near;
+        if (config?.far !== undefined) this.far = config.far;
+        this.updateProjectionMatrix();
+        this.updateWorldMatrix();
+    }
+
+    get worldMatrix(): mat4 {
+        return this._worldMatrix;
+    }
+
+    get invWorldMatrix(): mat4 {
+        return this._invWorldMatrix;
+    }
+
+    get projectionMatrix(): mat4 {
+        return this._projectionMatrix;
+    }
+
+    get invProjectionMatrix(): mat4 {
+        return this._invProjectionMatrix;
+    }
+
+    updateWorldMatrix(): void {
+        mat4.lookAt(this._worldMatrix, this.position, this.target, this.up);
+        mat4.invert(this._invWorldMatrix, this._worldMatrix);
+    }
+
+    updateProjectionMatrix(): void {
+        const fovy = this.fovx / this.aspect;
+        mat4.perspective(this._projectionMatrix, fovy, this.aspect, this.near, this.far);
+        mat4.invert(this._invProjectionMatrix, this._projectionMatrix);
+    }
+
+    rayCaster(): (ray: Ray, uv: ReadonlyVec2) => Ray {
+        const v4 = vec4.create();
+        return (ray, uv) => {
+            vec3.copy(ray.origin, vec4.transformMat4(v4, vec4.set(v4, 0, 0, 0, 1), this._invWorldMatrix) as vec3);
+            vec3.copy(ray.dir, vec4.transformMat4(v4, vec4.set(v4, uv[0], uv[1], 0, 1), this._invProjectionMatrix) as vec3);
+            vec3.copy(ray.dir, vec4.transformMat4(v4, vec4.set(v4, ray.dir[0], ray.dir[1], ray.dir[2], 0), this._invWorldMatrix) as vec3);
+            vec3.normalize(ray.dir, ray.dir);
+            return ray;
+        };
+    }
+}
