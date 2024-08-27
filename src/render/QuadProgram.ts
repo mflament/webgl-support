dae7717f6019a7399d5fcfebabeabcf32ce002e5
Marc <marcflament@free.fr>
2024-08-27 04:05:54 +0200

    Perhaps one day

--- src/render/QuadProgram.ts
+++ src/render/QuadProgram.ts
@@ -19,18 +19,17 @@ export async function createQuadProgramAsync(gl: WebGL2RenderingContext, props: 
 function sources(props: QuadProgramProps): ProgramSources {
     let vs;
     if ("vs" in props) vs = props.vs;
-    else vs = defaultQuadVS(props.positionAttribute);
+    else vs = defaultQuadVS(props.positionAttribute || "uv");
     return {vs, fs: props.fs};
 }
 
-function defaultQuadVS(posName = "uv") {
-    // language=glsl
-    return `#version 300 es
-    precision highp float;
-    layout(location = 0) in vec2 position;
-    out vec2 ${posName};
-    void main() {
-        ${posName} = (position.xy + 1.0) * 0.5;
-        gl_Position = vec4(position, 0.0, 1.0);
-    }`
+// language=glsl
+const defaultQuadVS = (posName: string) => `#version 300 es
+precision highp float;
+layout(location = 0) in vec2 position;
+out vec2 ${posName};
+void main() {
+    ${posName} = (position.xy + 1.0) * 0.5;
+    gl_Position = vec4(position, 0.0, 1.0);
 }
+`
